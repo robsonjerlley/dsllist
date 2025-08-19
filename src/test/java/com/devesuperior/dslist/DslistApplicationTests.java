04fda8a11d5c54980593031ad7b9e3000adf1ac6
Robson Jerlley <robsonjerlley.work@gmail.com>
2025-08-19 19:28:03 -0300

    projected created

---
+++ src/test/java/com/devesuperior/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
