6d162faedab094b2535df4ce63524c42da2b663b
Robson Jerlley <robsonjerlley.work@gmail.com>
2025-08-22 20:51:50 -0300

    GET by id

--- src/main/java/com/devesuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devesuperior/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devesuperior.dslist.controllers;
 
+import com.devesuperior.dslist.dto.GameDTO;
 import com.devesuperior.dslist.dto.GameMinDTO;
 import com.devesuperior.dslist.entities.Game;
 import com.devesuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,16 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+
+    }
+
     @GetMapping
-    public List<Game> findAll() {
-        List<Game> result = gameService.findAll();
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
         return result;
 
     }
--- src/main/java/com/devesuperior/dslist/services/GameService.java
+++ src/main/java/com/devesuperior/dslist/services/GameService.java
@@ -1,11 +1,13 @@
 package com.devesuperior.dslist.services;
 
+import com.devesuperior.dslist.dto.GameDTO;
 import com.devesuperior.dslist.dto.GameMinDTO;
 import com.devesuperior.dslist.entities.Game;
 import com.devesuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,10 +17,18 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
 
-    public List<Game> findAll(){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+
+    }
+
+    public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
-            return result;
+            return result.stream().map(GameMinDTO::new).toList();
     }
 
 
