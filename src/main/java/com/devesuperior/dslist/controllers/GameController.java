021eebf53bc2b829b2e7378466ff1ec1dd32fed1
Robson Jerlley <robsonjerlley.work@gmail.com>
2025-08-22 12:00:32 -0300

    Add class and packed  DTO, Controllers, Services

---
+++ src/main/java/com/devesuperior/dslist/controllers/GameController.java
@@ -0,0 +1,28 @@
+package com.devesuperior.dslist.controllers;
+
+import com.devesuperior.dslist.dto.GameMinDTO;
+import com.devesuperior.dslist.entities.Game;
+import com.devesuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<Game> findAll() {
+        List<Game> result = gameService.findAll();
+        return result;
+
+    }
+
+}
---
+++ src/main/java/com/devesuperior/dslist/services/GameService.java
@@ -0,0 +1,25 @@
+package com.devesuperior.dslist.services;
+
+import com.devesuperior.dslist.dto.GameMinDTO;
+import com.devesuperior.dslist.entities.Game;
+import com.devesuperior.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+
+    public List<Game> findAll(){
+
+    List<Game> result = gameRepository.findAll();
+        return result;
+    }
+
+
+}
